8a5fef2152fc74ae997b8d16c3467a22f8a80be1
imthe666st <imthe666st@gmail.com>
2016-04-30 23:42:39 +0200

    Added a Save / Load button

---
+++ NTRDebuggerTool/Objects/SaveManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace NTRDebuggerTool.Objects
+{
+    public class SaveManager
+    {
+        public String[] addr;
+        public int[] type;
+
+        public void Init()
+        {
+            if (addr == null) addr = new String[0];
+            if (type == null) type = new int[0];
+        }
+
+        public static void SaveToXml(string filePath, SaveManager sourceObj)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    System.Xml.Serialization.XmlSerializer xmlSerializer =
+                        new System.Xml.Serialization.XmlSerializer(sourceObj.GetType());
+                    xmlSerializer.Serialize(writer, sourceObj);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public static SaveManager LoadFromXml(string filePath)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    System.Xml.Serialization.XmlSerializer xmlSerializer =
+                        new System.Xml.Serialization.XmlSerializer(typeof(SaveManager));
+                    return (SaveManager)xmlSerializer.Deserialize(reader);
+                }
+            }
+            catch (Exception ex)
+            { }
+            return new SaveManager();
+        }
+    }
+}
